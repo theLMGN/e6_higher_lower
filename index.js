1ca9a8db8f4f2abf2428a2ccf1cbf4423d49f9fa
Leo Nesfield <leo@thelmgn.com>
2020-04-21 04:06:28 +0100

    make the game start

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// this code is very sloppy i wrote it at 3am
+
 (function() {
     var tags
     // get tags
@@ -10,9 +12,36 @@
         playing: false,
         score: 0,
         hiscore:0,
-        leftTag, rightTag,
+        leftTag: undefined,
+        rightTag: undefined,
     }
+
+    function random() {
+        var keys = Object.keys(tags)
+        var key = keys[Math.ceil(Math.random() * keys.length)]
+        return {tag:key,imgs: tags[key]}
+    }
+    function correct(left,right,higher) {
+        if (higher  & left <= right) { return true }
+        if (!higher & left >= right) { return true }
+        return false
+    }
+    function render() {
+        document.querySelector("#leftTagName").innerText = gamestate.leftTag.tag
+        document.querySelector("#leftTagResult").innerText = gamestate.leftTag.imgs
+        document.querySelector("#rightTagName").innerText = gamestate.rightTag.tag
+    }
+
+    console.log("here")
     window.playGame = function() {
+        if (!tags) return;
+        gamestate.leftTag = random()
+        gamestate.rightTag = random()
+        gamestate.playing = true
+        render()
+        document.querySelector("#introPanel").style.display = "none"
+        document.querySelector("#gamePanel").style.display = "block"
 
     }
+    document.querySelector("")
 })()
